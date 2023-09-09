3341607915641313e02a8a19aacd2e897910d5c2
tarun6575 <143867043+tarun6575@users.noreply.github.com>
2023-09-09 09:58:54 +0530

    Create defaultdemo.java

---
+++ defaultdemo.java
@@ -0,0 +1,16 @@
+import java.io.*;
+class Sample
+{
+    void display()
+    {
+        System.out.println("Hello world");
+    }
+}
+public class DefaultDemo
+{
+    public static void main(String[]args)
+    {
+        Sample s=new  Sample();
+        s.display();
+    }
+}
